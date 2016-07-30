3158244d22cff65233000e6cfa46ce81d3d24082
Padraic Calpin <padraic.calpin93@gmail.com>
2016-07-30 19:35:09 +0100

    Small filename change

---
+++ StabiliserRank/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <complex.h>
+#include <binaryform.h>
+#include <expsum.h>
+
+// void OuterProduct(int len, unsigned short *left, unsigned short *right, unsigned short **target)
+// {
+//     for (int i = 0; i < len; i++){
+//         for (int j = 0; j < len; j++){
+//             target[i][j] = 
+//         }
+//     }
+//     return;
+// }
+
+int main(){
+    //Do nothing
+    char a = 1;
+    char b = 2;
+    printf("%d\n",a^b);//Should equal 3
+}
