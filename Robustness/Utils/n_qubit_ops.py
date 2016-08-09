13f4801685bfe000c0ab3cae375ec3ebbf554773
Padraic Calpin <padraic.calpin93@gmail.com>
2016-08-09 17:32:27 +0100

    Tweak function names

--- Robustness/Utils/n_qubit_ops.py
+++ Robustness/Utils/n_qubit_ops.py
@@ -1,6 +1,8 @@
-"""Function to generate the full set of n-qubit operators with real-valued phase.
+"""Function to generate the full set of n-qubit stabiliser groups
+using the set of n-qubit paulis with real-valued phase.
 Note this set excludes the n-fold identity, and -1*n-fold identity."""
 from bitarray import bitarray
+from itertools import combinations
 from math import pow
 from qutip import qeye, sigmax, sigmaz, tensor
 
@@ -9,8 +11,10 @@ X = sigmax()
 Z = sigmaz()
 
 class BinarySubspace(object):
+    """Set-like class for bitarray objects to generate a closed subspace."""
     self._items = []
-    def __init__(*data):
+    self.generators = []
+    def __init__(self, *data):
         self.order = 0
         for val in data:
             if not isinstance(val, bitarray):
@@ -19,6 +23,7 @@ class BinarySubspace(object):
     
     def __contains__(self, it):
         for _el in self._items:
+            if all(it&_el):
                 return True
         return False
 
@@ -27,7 +32,7 @@ class BinarySubspace(object):
             yield item
 
     def _generate(self, obj):
-        for item in self_items:
+        for item in self._items:
             new = item^obj
             if new in self:
                 continue
@@ -39,6 +44,7 @@ class BinarySubspace(object):
 
     def _append(self, obj):
         self.order +=1
+        self.generators.append(obj)
         self._items.append(obj)
         self._generate(obj)
 
@@ -67,20 +73,23 @@ def string_to_pauli(n, bits):
 
 def find_generators(n, bitstrings):
     subspaces = []
-    generators = []
     for pair in combinations(bitstrings, 2):
         candidate = BinarySubspace(pair[0], pair[1])
         for subspace in subspaces:
             if candidate == subspace:
                 continue
         subspaces.append(candidate)
-        generators.append(pair)
-    return generators
+    return [tuple(subspace.generators) for subspace in subspaces]
 
-def gen_operators(n):
-    bits = []
+def gen_stabiliser_groups(n):
+    bitstrings = []
     for i in range(2, pow(2,n)): #We ignore the strings for 0 and 1 as these correspond to I^{n} and -I^{n}.
         bin_string = bin(i)[2:] #strip the 0b from the string
         a = bitarray(n-len(bin_string)) #leftpad with 0s
         a.extend(bin_string)
-        bits.append(a)
+        bitstrings.append(a)
+    generators = find_generators(n, bitstrings)
+    pauli_pairs = []
+    for pair in generators:
+        p1, p2 = pair
+        pauli_pairs.append((string_to_pauli(p1), string_to_pauli(p2)))
